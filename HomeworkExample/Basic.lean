289d85a6f282a18a15f433786651b5edc7e5ef86
AydenLamp <ayden@lamparski.com>
2026-01-11 21:32:44 +0000

    renamed example

--- HomeworkExample/Basic.lean
+++ HomeworkExample/Basic.lean
@@ -4,7 +4,7 @@ open Nat
 
 variable {m n : ℕ}
 
-example (h : m ≤ n) : (n - m) ^ 2 = n ^ 2 + m ^ 2 - 2 * n * m := by
+theorem sub_le_pow_two_eq (h : m ≤ n) : (n - m) ^ 2 = n ^ 2 + m ^ 2 - 2 * n * m := by
   rcases Nat.le.dest h with ⟨k, hk⟩
   simp [← hk, pow_two, add_mul, mul_add]
   ring_nf
