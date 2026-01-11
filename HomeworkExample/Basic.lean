69dac65691eaa5d6777f237d296a5541f51b4ed3
AydenLamp <ayden@lamparski.com>
2026-01-11 21:31:04 +0000

    solved example

--- HomeworkExample/Basic.lean
+++ HomeworkExample/Basic.lean
@@ -1,6 +1,12 @@
 import Mathlib
 
-variable (a b : ℕ)
+open Nat
 
-example (h : b ≤ a) : (a - b) ^ 2 = a ^ 2 + b ^2 - 2 * a * b := by
-  sorry
+variable {m n : ℕ}
+
+example (h : m ≤ n) : (n - m) ^ 2 = n ^ 2 + m ^ 2 - 2 * n * m := by
+  rcases Nat.le.dest h with ⟨k, hk⟩
+  simp [← hk, pow_two, add_mul, mul_add]
+  ring_nf
+  rw [add_comm (k * m * 2)]
+  simp [add_assoc]
